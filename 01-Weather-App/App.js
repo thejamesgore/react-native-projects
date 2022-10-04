5b249c9cc3af4e00c579d9ec3c64284b3f894265
James Gore <83005220+thejamesgore@users.noreply.github.com>
2022-10-04 12:04:39 +0100

    added environment variables

--- 01-Weather-App/App.js
+++ 01-Weather-App/App.js
@@ -2,6 +2,9 @@ import { StatusBar } from 'expo-status-bar'
 import React, { useEffect, useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import * as Location from 'expo-location'
+import { API_KEY } from 'react-native-dotenv'
+
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?'
 
 export default function App() {
   const [location, setLocation] = useState(null)
@@ -17,6 +20,9 @@ export default function App() {
       }
 
       let location = await Location.getCurrentPositionAsync({})
+
+      const { latitude, longitude } = location.coords
+      alert(`Lat: ${latitude}, Longitude ${latitude}`)
       setLocation(location)
     })()
   }, [])
