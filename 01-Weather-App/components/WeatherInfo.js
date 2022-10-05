aadb9aa60169b6bb45e313ec537b186c664ee2cc
James Gore <83005220+thejamesgore@users.noreply.github.com>
2022-10-05 17:10:55 +0100

    added weather image

--- 01-Weather-App/components/WeatherInfo.js
+++ 01-Weather-App/components/WeatherInfo.js
@@ -1,13 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
 
 const WeatherInfo = ({ weather }) => {
   const {
     main: { temp },
+    weather: [details],
   } = weather
 
+  const { icon } = details
+  const iconUrl = `https://openweathermap.org/img/wn/${icon}@4x.png`
+
   return (
-    <View>
+    <View style={styles.weatherInfo}>
+      <Image style={styles.weatherIcon} source={{ uri: iconUrl }} />
       <Text>{temp}</Text>
     </View>
   )
@@ -18,5 +23,9 @@ export default WeatherInfo
 const styles = StyleSheet.create({
   weatherInfo: {
     alignItems: 'center',
+  },
+  weatherIcon: {
+    width: 100,
+    height: 100,
   },
 })
