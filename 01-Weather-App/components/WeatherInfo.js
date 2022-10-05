51c909de4050a74d49520b091a6e418a8a354207
James Gore <83005220+thejamesgore@users.noreply.github.com>
2022-10-05 17:18:29 +0100

    added project colours in utils

--- 01-Weather-App/components/WeatherInfo.js
+++ 01-Weather-App/components/WeatherInfo.js
@@ -1,5 +1,8 @@
 import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
+import { colors } from '../utils/index'
+
+const { PRIMARY_COLOR, SECONDARY_COLOR } = colors
 
 const WeatherInfo = ({ weather }) => {
   const {
