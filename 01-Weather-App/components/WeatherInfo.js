963500a5c5e264da02f1ac90e95c478a05de3d98
James Gore <83005220+thejamesgore@users.noreply.github.com>
2022-10-05 17:15:14 +0100

    finished structure of weatherInfo component

--- 01-Weather-App/components/WeatherInfo.js
+++ 01-Weather-App/components/WeatherInfo.js
@@ -5,15 +5,19 @@ const WeatherInfo = ({ weather }) => {
   const {
     main: { temp },
     weather: [details],
+    name,
   } = weather
 
-  const { icon } = details
+  const { icon, main, description } = details
   const iconUrl = `https://openweathermap.org/img/wn/${icon}@4x.png`
 
   return (
     <View style={styles.weatherInfo}>
+      <Text>{name}</Text>
       <Image style={styles.weatherIcon} source={{ uri: iconUrl }} />
       <Text>{temp}</Text>
+      <Text style={styles.weatherDescritpion}>{description}</Text>
+      <Text>{main}</Text>
     </View>
   )
 }
@@ -23,6 +27,9 @@ export default WeatherInfo
 const styles = StyleSheet.create({
   weatherInfo: {
     alignItems: 'center',
+  },
+  weatherDescritpion: {
+    textTransform: 'capitalize',
   },
   weatherIcon: {
     width: 100,
