8fe8b03168eadccf29cd69661d89ada8b217ebde
James Gore <83005220+thejamesgore@users.noreply.github.com>
2022-10-06 14:38:24 +0100

    added weather details stylings

--- 01-Weather-App/components/WeatherDetails.js
+++ 01-Weather-App/components/WeatherDetails.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { colors } from '../utils/index'
+import { FontAwesome5, MaterialCommunityIcons } from '@expo/vector-icons'
 
 const { PRIMARY_COLOR, SECONDARY_COLOR, BORDER_COLOR } = colors
 
@@ -12,11 +13,27 @@ export default function WeatherDetails({ weather }) {
   return (
     <View style={styles.weatherDetails}>
       <View style={styles.weatherDetailsRow}>
-        <View style={styles.weatherDetailsBox}>
-          <Text>{feels_like}</Text>
+        <View
+          style={{
+            ...styles.weatherDetailsBox,
+            borderRightWidth: 1,
+            borderRightColor: BORDER_COLOR,
+          }}
+        >
+          <View style={styles.weatherDetailsRow}>
+            <FontAwesome5
+              name="temperature-low"
+              size={25}
+              color={PRIMARY_COLOR}
+            />
+            <View style={styles.weatherDetailsItems}>
+              <Text>Feels like</Text>
+              <Text style={styles.textSecondary}>{feels_like}</Text>
+            </View>
+          </View>
         </View>
         <View style={styles.weatherDetailsBox}>
-          <Text>{feels_like}</Text>
+          <Text>{humidity}</Text>
         </View>
       </View>
     </View>
@@ -39,5 +56,15 @@ const styles = StyleSheet.create({
   weatherDetailsBox: {
     flex: 1,
     padding: 20,
+  },
+  weatherDetailsItems: {
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+  },
+  textSecondary: {
+    fontSize: 15,
+    color: SECONDARY_COLOR,
+    fontWeight: '700',
+    margin: 7,
   },
 })
