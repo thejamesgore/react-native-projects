acf0c8d65b5c61603495d9e7c486a625cc2ecc76
James Gore <83005220+thejamesgore@users.noreply.github.com>
2022-10-08 19:08:06 +0100

    tweaked styling

--- 02-Login-App/screens/LoginScreen.js
+++ 02-Login-App/screens/LoginScreen.js
@@ -10,31 +10,28 @@ import React from 'react'
 
 const LoginScreen = () => {
   return (
-    <KeyboardAvoidingView style={styles.container}>
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
       <View style={styles.inputContainer}>
         <TextInput
           placeholder="Email"
-          // value={}
-          // onChangeText={text =>}
+          //   value={email}
+          //   onChangeText={(text) => setEmail(text)}
           style={styles.input}
         />
         <TextInput
           placeholder="Password"
-          // value={}
-          // onChangeText={text =>}
+          //   value={password}
+          //   onChangeText={(text) => setPassword(text)}
           style={styles.input}
           secureTextEntry
         />
       </View>
 
       <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={() => {}} style={styles.button}>
+        <TouchableOpacity style={styles.button}>
           <Text style={styles.buttonText}>Login</Text>
         </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => {}}
-          style={[styles.button, styles.buttonOutline]}
-        >
+        <TouchableOpacity style={[styles.button, styles.buttonOutline]}>
           <Text style={styles.buttonOutlineText}>Register</Text>
         </TouchableOpacity>
       </View>
@@ -60,28 +57,33 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginTop: 5,
   },
+  buttonContainer: {
+    width: '60%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 40,
+  },
   button: {
-    backgroundColor: '#0782f9',
+    backgroundColor: '#0782F9',
     width: '100%',
     padding: 15,
     borderRadius: 10,
     alignItems: 'center',
+  },
+  buttonOutline: {
+    backgroundColor: 'white',
+    marginTop: 5,
+    borderColor: '#0782F9',
+    borderWidth: 2,
   },
   buttonText: {
     color: 'white',
     fontWeight: '700',
     fontSize: 16,
   },
-  buttonContainer: {
-    width: '60%',
-    justifyContent: 'center',
-    alignItems: 'center',
+  buttonOutlineText: {
+    color: '#0782F9',
+    fontWeight: '700',
+    fontSize: 16,
   },
-  buttonOutline: {
-    backgroundColor: 'white',
-    marginTop: 5,
-    borderColor: '#0782f9',
-    borderWidth: 2,
-  },
-  buttonOutlineText: { color: '#0782f9', fontWeight: '700', fontSize: 16 },
 })
