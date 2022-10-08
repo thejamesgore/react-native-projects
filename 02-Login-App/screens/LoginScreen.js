c8b45a8a738b4a1a0915a8414135f8017a792999
James Gore <83005220+thejamesgore@users.noreply.github.com>
2022-10-08 19:05:18 +0100

    added LoginScreen styling

--- 02-Login-App/screens/LoginScreen.js
+++ 02-Login-App/screens/LoginScreen.js
@@ -50,12 +50,38 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-
-  inputContainer: {},
-  button: {},
-  buttonText: {},
-  input: {},
-  buttonContainer: {},
-  buttonOutline: {},
-  buttonOutlineText: {},
+  inputContainer: {
+    width: '80%',
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  button: {
+    backgroundColor: '#0782f9',
+    width: '100%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+  buttonContainer: {
+    width: '60%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonOutline: {
+    backgroundColor: 'white',
+    marginTop: 5,
+    borderColor: '#0782f9',
+    borderWidth: 2,
+  },
+  buttonOutlineText: { color: '#0782f9', fontWeight: '700', fontSize: 16 },
 })
