192f3ef0838a1f60042ecaf99fe30d68dd3f4c25
James Gore <83005220+thejamesgore@users.noreply.github.com>
2022-10-08 18:38:22 +0100

    fleshed out loginscreen

--- 02-Login-App/screens/LoginScreen.js
+++ 02-Login-App/screens/LoginScreen.js
@@ -1,14 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native'
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native'
 import React from 'react'
 
 const LoginScreen = () => {
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
+    <KeyboardAvoidingView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Email"
+          // value={}
+          // onChangeText={text =>}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Password"
+          // value={}
+          // onChangeText={text =>}
+          style={styles.input}
+          secureTextEntry
+        />
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={() => {}}
+          style={[styles.button, styles.buttonOutline]}
+        >
+          <Text style={styles.button}>Register</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   )
 }
 
 export default LoginScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
