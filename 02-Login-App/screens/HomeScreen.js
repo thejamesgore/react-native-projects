8e013fd110040c00161d3ac9eac0ebb01cd0d811
James Gore <83005220+thejamesgore@users.noreply.github.com>
2022-10-09 01:36:54 +0100

    added signout function

--- 02-Login-App/screens/HomeScreen.js
+++ 02-Login-App/screens/HomeScreen.js
@@ -1,11 +1,24 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import { auth } from '../firebase'
+import { useNavigation } from '@react-navigation/native'
 
 const HomeScreen = () => {
+  const navigation = useNavigation()
+
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        navigation.replace('Login')
+      })
+      .catch((error) => alert(error.message))
+  }
+
   return (
     <View style={styles.container}>
-      <Text>You're logged in!</Text>
-      <TouchableOpacity style={styles.button}>
+      <Text>You're logged in! {auth.currentUser?.email}</Text>
+      <TouchableOpacity onPress={handleSignOut} style={styles.button}>
         <Text style={styles.buttonText}>Sign out</Text>
       </TouchableOpacity>
     </View>
