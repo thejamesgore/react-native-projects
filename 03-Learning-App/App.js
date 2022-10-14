bada7d26833682659e11d72a5b304b324459bef4
James Gore <83005220+thejamesgore@users.noreply.github.com>
2022-10-14 22:03:20 +0100

    added state

--- 03-Learning-App/App.js
+++ 03-Learning-App/App.js
@@ -1,9 +1,17 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
 
 import hiragana from './japanese/Characters'
 
 export default function App() {
+  // Declaring our states
+
+  const [input, setInput] = useState('')
+  const [currentCharacter, setCurrentCharacter] = useState(0)
+  const [streak, setStreak] = useState(0)
+  const [error, setError] = useState(false)
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
