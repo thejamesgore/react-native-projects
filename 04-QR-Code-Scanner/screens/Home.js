87ddf57e1515d210ea35ee881d50205ef83fcae3
James Gore <83005220+thejamesgore@users.noreply.github.com>
2022-10-17 13:18:05 +0100

    added scanner page

--- 04-QR-Code-Scanner/screens/Home.js
+++ 04-QR-Code-Scanner/screens/Home.js
@@ -1,14 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Button } from 'react-native'
 import React from 'react'
 
+import { useNavigation } from '@react-navigation/native'
+
 const Home = () => {
+  const navigation = useNavigation()
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.buttonContainer}>
+      <Text>Hello</Text>
+      <Button
+        onPress={() => navigation.navigate('Scanner')}
+        title="Scan QR Code"
+      />
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+})
--- 04-QR-Code-Scanner/screens/Scanner.js
+++ 04-QR-Code-Scanner/screens/Scanner.js
@@ -1,7 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Linking } from 'react-native'
+import { barCodeScanner } from 'expo-barcode-scanner'
+import React, { useState } from 'react'
+import { useEffect } from 'react'
 
 const Scanner = () => {
+  const [hasCamera, setHasCamera] = useState(null)
+  const [scanned, setScanned] = useState(false)
+
+  useEffect(() => {
+    ;(async () => {
+      const { status } = await barCodeScanner.requestPermissionsAsync()
+      setHasCamera(status === 'granted')
+    })()
+  }, [])
+
+  const handleScan = ({ type, data }) => {}
+
   return (
     <View>
       <Text>Scanner</Text>
