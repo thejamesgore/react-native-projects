37cc17e95e58059bc2880815a2aab59376e891e1
James Gore <83005220+thejamesgore@users.noreply.github.com>
2022-12-30 14:34:22 +0000

    to deply

--- 07-Lightning-Wallet/App.js
+++ 07-Lightning-Wallet/App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import { WebView } from 'react-native-webview'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={{ flex: 1, marginTop: 30, backgroundColor: '#000' }}>
+      <WebView source={{ uri: 'https://lightning-wallet.vercel.app/' }} />
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
